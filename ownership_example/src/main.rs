9787431c79b251537c0d4c7d291ad66f482b0f80
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-08-06 17:23:53 -0300

    Commenting code

--- ownership_example/src/main.rs
+++ ownership_example/src/main.rs
@@ -22,12 +22,12 @@ fn calculate_length(s: &String) -> usize {
 
 
 // 3- When the owner goes out of scope, the value will be dropped
-fn main() {
-    let s1 = String::from("RUST");  
-    let len = calculate_length(&s1);
-    println!("Lenght of '{}' is {}.", s1, len);
-}
+// fn main() {
+//     let s1 = String::from("RUST");  
+//     let len = calculate_length(&s1);
+//     println!("Lenght of '{}' is {}.", s1, len);
+// }
 
-fn print_lost(s: &string) {
-    println!("{}", s1);
-}
+// fn print_lost(s: &string) {
+//     println!("{}", s1);
+// }
