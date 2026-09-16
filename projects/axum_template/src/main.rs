02fe6c829d298ac7c8ab5471ae8b68ee532e156f
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-09-16 16:57:48 -0300

    Initializing project with simple router and test

--- projects/axum_template/src/main.rs
+++ projects/axum_template/src/main.rs
@@ -1,6 +1,19 @@
 #![allow(unused)] //For begenning only
 
+use std::net::SocketAddr;
+
+use axum::{Router, response::Html, routing::get};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+   let routers = Router::new().route(
+    "/hello",
+    get(|| async {Html("Hello <strong>World!!!</strong>")}),
+   );
+
+   let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+   println!("LISTENING on {:?}\n", addr);
+   axum::Server::bind(&addr)
+    .serve(routers.into_make_service())
+    .await.unwrap()
 }
